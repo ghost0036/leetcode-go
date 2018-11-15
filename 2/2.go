a71f1cd35b34e8bc6cc06f09717df5f864d76581
lushengpeng <lushengpeng@migu.cn>
2018-11-15 11:47:05 +0800

    pass  两数相加

--- 2/2.go
+++ 2/2.go
@@ -13,10 +13,64 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/**
+错误解答；无法解决溢出问题
+*/
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	sum1 := GetSum(l1)
 	sum2 := GetSum(l2)
 	return GetNode(sum1 + sum2)
+}
+
+/**
+通过
+*/
+func addTwoNumbers1(l1 *ListNode, l2 *ListNode) *ListNode {
+	result := &ListNode{}
+
+	cur := result
+
+	list1 := l1
+	list2 := l2
+	isNeed := 0
+	for list1 != nil || list2 != nil {
+		var node ListNode
+		v1 := 0
+		v2 := 0
+
+		if list1 != nil {
+			v1 = list1.Val
+			list1 = list1.Next
+		}
+
+		if list2 != nil {
+			v2 = list2.Val
+			list2 = list2.Next
+		}
+
+		sum := v1 + v2 + isNeed
+
+		if sum > 9 {
+			isNeed = 1
+		} else {
+			isNeed = 0
+		}
+		node.Val = sum % 10
+
+		cur.Next = &node
+		cur = &node
+	}
+
+	if isNeed == 1 {
+		cur.Next = &ListNode{1, nil}
+	}
+
+	//if cur.Val >= 10 {
+	//	cur.Val = cur.Val % 10
+	//	cur.Next = &ListNode{1,nil}
+	//}
+
+	return result.Next
 }
 
 func GetSum(l *ListNode) int {
--- 2/2_test.go
+++ 2/2_test.go
@@ -9,14 +9,31 @@ func TestNomal(t *testing.T) {
 	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
 	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 
-	s := addTwoNumbers(l1, l2)
-	fmt.Println(s.Val, s.Next.Val, s.Next.Val)
+	//s := addTwoNumbers(l1, l2)
+	s := addTwoNumbers1(l1, l2)
+	fmt.Println(s.Val, s.Next.Val, s.Next.Next.Val)
 }
 
 func TestException(t *testing.T) {
-	l1 := &ListNode{0, nil}
+	l1 := &ListNode{1, &ListNode{8, nil}}
 	l2 := &ListNode{0, nil}
 
-	s := addTwoNumbers(l1, l2)
-	fmt.Println(s.Val, s.Next.Val, s.Next.Val)
+	s := addTwoNumbers1(l1, l2)
+	fmt.Println(s.Val, s.Next.Val)
+}
+
+func TestException1(t *testing.T) {
+	l1 := &ListNode{5, nil}
+	l2 := &ListNode{5, nil}
+
+	s := addTwoNumbers1(l1, l2)
+	fmt.Println(s.Val, s.Next.Val)
+}
+
+func TestException2(t *testing.T) {
+	l1 := &ListNode{1, nil}
+	l2 := &ListNode{9, &ListNode{9, nil}}
+
+	s := addTwoNumbers1(l1, l2)
+	fmt.Println(s.Val, s.Next.Val, s.Next.Next.Val)
 }
